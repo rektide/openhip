35bd6d71c4b00248fa3bf1f52df6c5245acf4ebf
siliconja <siliconja@4c5cb64f-9889-4596-9799-84b02dc3effa>
2009-08-13 20:50:12 +0000

    remove use of libtool from autoconf and special Mac OS X case
    add ./bootstrap.sh --enable-sma-crawler option to get libtool back for ./configure --enable-sma-crawler 
    
    
    git-svn-id: http://svn.code.sf.net/p/openhip/code/hip/trunk@26 4c5cb64f-9889-4596-9799-84b02dc3effa

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,43 @@
 #!/bin/sh
+
+# libtool is only used to build the configuration libraries associated
+#  with the --enable-sma-crawler configure option
+if [ "$1a" = "--enable-sma-crawlera" ]; then
+	LIBTOOLIZE_MSG="echo Running libtoolize..."
+	LIBTOOLIZE="libtoolize --force --copy --automake"
+	CONFOPTS=" --enable-sma-crawler"
+	if [ -e src/util/Makefile.am.disabled ]; then
+		mv src/util/Makefile.am.disabled src/util/Makefile.am
+	fi
+	mv configure.ac configure.ac.orig
+	sed -e "s,#AC_PROG_LIBTOOL,AC_PROG_LIBTOOL," configure.ac.orig > configure.ac
+elif [ "$1a" = "a" ]; then
+	LIBTOOLIZE_MSG=""
+	LIBTOOLIZE=""
+	CONFOPTS=""
+	if [ ! -e src/util/Makefile.am.disabled ]; then
+		mv src/util/Makefile.am src/util/Makefile.am.disabled
+		touch src/util/Makefile.am
+	fi
+	if [ -e configure.ac.orig ]; then
+		mv configure.ac.orig configure.ac
+	fi
+else
+	echo "usage: ./bootstrap.sh [--enable-sma-crawler]"
+	exit 1;
+fi
+
 if [ -d /usr/local/share/aclocal ]; then
 	EXTRA_INC="-I /usr/local/share/aclocal"
 else
 	EXTRA_INC=""
 fi;
-LIBTOOLIZE="libtoolize --force --copy --automake"
-if [ `uname` = Darwin ]; then
-	echo "Using Mac OS X Makefile."
-	mv -n src/Makefile.am src/Makefile.orig.am
-	cp src/Makefile.mac.am src/Makefile.am
-	# don't run libtoolize on Mac
-	LIBTOOLIZE="echo -n ' '"
-fi;
 
 echo "Running aclocal..." && aclocal $EXTRA_INC \
-    && echo "Running libtoolize..." && $LIBTOOLIZE \
+    && $LIBTOOLIZE_MSG && $LIBTOOLIZE \
     && echo "Running automake..." && automake --add-missing --copy --foreign \
     && echo "Running autoconf..." && autoconf
 
 echo ""
-echo "You are now ready to run \"./configure\"."
+echo "You are now ready to run \"./configure\"$CONFOPTS."
 
