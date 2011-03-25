fde099f26f8fe56e6a5a0a639c90d7a38fd2496b
siliconja <siliconja@4c5cb64f-9889-4596-9799-84b02dc3effa>
2011-03-25 16:54:36 +0000

    remove GPL-encumbered ASM checksumming code, use inline C instead; these were shown to be slightly faster with -O3 optimizations on x86
    note: this file could use further cleanup
    
    
    
    git-svn-id: http://svn.code.sf.net/p/openhip/code/hip/trunk@71 4c5cb64f-9889-4596-9799-84b02dc3effa

--- src/include/win32/checksum.h
+++ src/include/win32/checksum.h
@@ -1,27 +1,6 @@
 /*
- * This file contains a combination of:
- * linux/include/asm-i386/checksum.h and
- * linux/include/net/checksum.h from the Linux 2.6.8.1 kernel,
- * plus HIP checksum modifications.
- * 
- */
-
-/*
- * INET		An implementation of the TCP/IP protocol suite for the LINUX
- *		operating system.  INET is implemented using the  BSD Socket
- *		interface as the means of communication with the user level.
- *
- *		Checksumming functions for IP, TCP, UDP and so on
- *
- * Authors:	Jorge Cwik, <jorge@laser.satlink.net>
- *		Arnt Gulbrandsen, <agulbra@nvg.unit.no>
- *		Borrows very liberally from tcp.c and ip.c, see those
- *		files for more names.
- *
- *		This program is free software; you can redistribute it and/or
- *		modify it under the terms of the GNU General Public License
- *		as published by the Free Software Foundation; either version
- *		2 of the License, or (at your option) any later version.
+ * Host Identity Protocol
+ * Copyright (C) 2002-2011 the Boeing Company
  */
 
 #ifndef _CHECKSUM_H
@@ -45,20 +24,6 @@ struct my_in6_addr {
 };
 #endif
 
-/*
- * the following inlines are from linux/include/asm-i386/checksum.h
- */
-
-/*
- *	This is a version of ip_compute_csum() optimized for IP headers,
- *	which always checksum on 4 octet boundaries.
- *
- *	By Jorge Cwik <jorge@laser.satlink.net>, adapted for linux by
- *	Arnt Gulbrandsen.
- */
-#ifdef __WIN32__
-/* This isn't the 'fast' checksum, since the GCC inline ASM version is not 
- * available in Windows; this is the same code from hip_util.c */
 static __inline unsigned short ip_fast_csum(unsigned char * iph,
 					  unsigned int ihl)
 {
@@ -89,69 +54,16 @@ static __inline unsigned short ip_fast_csum(unsigned char * iph,
     
 	return(checksum);
 }
-#else
-static inline unsigned short ip_fast_csum(unsigned char * iph,
-					  unsigned int ihl)
-{
-	unsigned int sum;
 
-	__asm__ __volatile__(
-	    "movl (%1), %0	;\n"
-	    "subl $4, %2	;\n"
-	    "jbe 2f		;\n"
-	    "addl 4(%1), %0	;\n"
-	    "adcl 8(%1), %0	;\n"
-	    "adcl 12(%1), %0	;\n"
-"1:	    adcl 16(%1), %0	;\n"
-	    "lea 4(%1), %1	;\n"
-	    "decl %2		;\n"
-	    "jne 1b		;\n"
-	    "adcl $0, %0	;\n"
-	    "movl %0, %2	;\n"
-	    "shrl $16, %0	;\n"
-	    "addw %w2, %w0	;\n"
-	    "adcl $0, %0	;\n"
-	    "notl %0		;\n"
-"2:				;\n"
-	/* Since the input registers which are loaded with iph and ipl
-	   are modified, we must also specify them as outputs, or gcc
-	   will assume they contain their original values. */
-	: "=r" (sum), "=r" (iph), "=r" (ihl)
-	: "1" (iph), "2" (ihl)
-	: "memory");
-	return(sum);
-}
-#endif
-
-/*
- *	Fold a partial checksum
- */
-
-#ifdef __WIN32__
 static __inline unsigned int csum_fold(unsigned int sum)
 {
-	/* this is from hip_util.c - checksum_packet() */
 	/*  Fold 32-bit sum to 16 bits */
 	while (sum>>16)
 		sum = (sum & 0xffff) + (sum >> 16);
-	/* take the one's complement of the sum */ 
+	/* take the one's complement of the sum */
 	return((__u16)~sum);
 }
-#else
 
-static inline unsigned int csum_fold(unsigned int sum)
-{
-	__asm__(
-		"addl %1, %0		;\n"
-		"adcl $0xffff, %0	;\n"
-		: "=r" (sum)
-		: "r" (sum << 16), "0" (sum & 0xffff0000)
-	);
-	return (~sum) >> 16;
-}
-#endif
-
-#ifdef __WIN32__
 static __inline unsigned int csum_add(unsigned int csum, unsigned int addend);
 static __inline unsigned long csum_tcpudp_nofold(unsigned long saddr,
 						   unsigned long daddr,
@@ -165,24 +77,6 @@ static __inline unsigned long csum_tcpudp_nofold(unsigned long saddr,
 	sum = csum_add(sum, (__u32)proto);
 	return(sum);	
 }
-#else
-
-static inline unsigned long csum_tcpudp_nofold(unsigned long saddr,
-						   unsigned long daddr,
-						   unsigned short len,
-						   unsigned short proto,
-						   unsigned int sum)
-{
-    __asm__(
-	"addl %1, %0	;\n"
-	"adcl %2, %0	;\n"
-	"adcl %3, %0	;\n"
-	"adcl $0, %0	;\n"
-	: "=r" (sum)
-	: "g" (daddr), "g"(saddr), "g"((ntohs(len)<<16)+proto*256), "0"(sum));
-    return sum;
-}
-#endif
 
 /*
  * computes the checksum of the TCP/UDP pseudo-header
@@ -205,53 +99,17 @@ static inline unsigned short int csum_tcpudp_magic(unsigned long saddr,
 	return csum_fold(csum_tcpudp_nofold(saddr,daddr,len,proto,sum));
 }
 
-#ifndef __WIN32__
-#define _HAVE_ARCH_IPV6_CSUM
-static __inline__ unsigned short int csum_ipv6_magic(struct in6_addr *saddr,
-						     struct in6_addr *daddr,
-						     __u32 len,
-						     unsigned short proto,
-						     unsigned int sum)
-{
-	__asm__(
-		"addl 0(%1), %0		;\n"
-		"adcl 4(%1), %0		;\n"
-		"adcl 8(%1), %0		;\n"
-		"adcl 12(%1), %0	;\n"
-		"adcl 0(%2), %0		;\n"
-		"adcl 4(%2), %0		;\n"
-		"adcl 8(%2), %0		;\n"
-		"adcl 12(%2), %0	;\n"
-		"adcl %3, %0		;\n"
-		"adcl %4, %0		;\n"
-		"adcl $0, %0		;\n"
-		: "=&r" (sum)
-		: "r" (saddr), "r" (daddr),
-		  "r"(htonl(len)), "r"(htonl(proto)), "0"(sum));
-
-	return csum_fold(sum);
-}
-#endif
-
 /*
  * the following inlines are from linux/include/net/checksum.h
  */
 
-#ifdef __WIN32__
 static __inline unsigned int csum_add(unsigned int csum, unsigned int addend)
-#else
-static inline unsigned int csum_add(unsigned int csum, unsigned int addend)
-#endif
 {
 	csum += addend;
 	return csum + (csum < addend);
 }
 
-#ifdef __WIN32__
 static __inline unsigned int csum_sub(unsigned int csum, unsigned int addend)
-#else
-static inline unsigned int csum_sub(unsigned int csum, unsigned int addend)
-#endif
 {
 	return csum_add(csum, ~addend);
 }
@@ -259,17 +117,10 @@ static inline unsigned int csum_sub(unsigned int csum, unsigned int addend)
 /* 
  * HIP checksum = tcp checksum + hitMagic - csum(saddr,daddr)
  */
-#ifdef __WIN32__
 static __inline unsigned short csum_tcpudp_hip_nofold(unsigned long saddr,
 						   unsigned long daddr,
 						   unsigned short sum,
 						   unsigned short  hitMagic)
-#else
-static inline unsigned short csum_tcpudp_hip_nofold(unsigned long saddr,
-						   unsigned long daddr,
-						   unsigned short sum,
-						   unsigned short  hitMagic)
-#endif
 {
 	/* sum is assumed to be the folded complement, so get the sum back */
 	unsigned short ret = ~sum;
@@ -280,17 +131,10 @@ static inline unsigned short csum_tcpudp_hip_nofold(unsigned long saddr,
 	return ret;
 }
 
-#ifdef __WIN32__
 static __inline unsigned short csum_hip_revert(unsigned long saddr,
 						   unsigned long daddr,
 						   unsigned short sum,
 						   unsigned short  hitMagic)
-#else
-static inline unsigned short csum_hip_revert(unsigned long saddr,
-						   unsigned long daddr,
-						   unsigned short sum,
-						   unsigned short  hitMagic)
-#endif
 {
 	/* sum is assumed to be the folded complement, so get the sum back */
 	unsigned short ret = ~sum;
