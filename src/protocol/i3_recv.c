06de676f1801cd4156a381f3d2f2e15d64267686
tom_henderson <tom_henderson@4c5cb64f-9889-4596-9799-84b02dc3effa>
2011-07-26 16:13:17 +0000

    missing licenses
    git-svn-id: http://svn.code.sf.net/p/openhip/code/hip/trunk@136 4c5cb64f-9889-4596-9799-84b02dc3effa

--- src/protocol/i3_recv.c
+++ src/protocol/i3_recv.c
@@ -1,10 +1,22 @@
 /*
- * Implementation of Hi3 receiver
- * Author: (c) Andrei Gurtov, HIIT
- * Licence: GPLv2
+ * Copyright (C) 2005 Andrei Gurtov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  Authors:    Andrei Gurtov, HIIT
+ *  Written: 3.3.2005
+ *
+ * \brief Implementation of Hi3 receiver
  * Based on a modified example from UCB i3
- * Written: 3.3.2005
-*/
+ */
 
 #include <hip/i3_hip.h>
 
--- src/protocol/i3_send.c
+++ src/protocol/i3_send.c
@@ -1,10 +1,22 @@
 /*
- * Implementation of Hi3 sender
- * Author: (c) Andrei Gurtov, HIIT
- * Licence: GPLv2
- * Modified example from UCB i3
- * Written: 10.2.2005
-*/
+ * Copyright (C) 2005 Andrei Gurtov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  Authors:    Andrei Gurtov, HIIT
+ *  Written: 10.2.2005
+ *
+ * \brief Implementation of Hi3 sender
+ * Based on a modified example from UCB i3
+ */
 
 #include <hip/i3_hip.h>
 
