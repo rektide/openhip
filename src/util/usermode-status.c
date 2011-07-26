166d6f1760a24f32b7dc7d9f0e92334e672ab7e1
tom_henderson <tom_henderson@4c5cb64f-9889-4596-9799-84b02dc3effa>
2011-07-26 16:03:33 +0000

    missing license on some utils
    git-svn-id: http://svn.code.sf.net/p/openhip/code/hip/trunk@132 4c5cb64f-9889-4596-9799-84b02dc3effa

--- src/util/usermode-status.c
+++ src/util/usermode-status.c
@@ -1,3 +1,19 @@
+/*
+ * Host Identity Protocol
+ * Copyright (C) 2009 the Boeing Company
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #include <stdio.h>
 #ifndef __WIN32__
 #include <unistd.h>
